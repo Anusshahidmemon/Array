a5097af0c5c7d659091a2bc5e446ed93cd6fedce
Anusshahidmemon <shahidanus21@gmail.com>
2024-02-29 03:14:24 -0800

    array assignment

---
+++ app.js
@@ -0,0 +1,37 @@
+let integers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+console.log(integers)
+
+
+let newintegers = integers.slice(5,16)
+console.log(integers)
+console.log(newintegers)
+
+let name = ["Anus","Shahid","Salman","Ahmed","mushtaq"]
+console.log(name)
+let oneName = name.shift()
+name.unshift("ayesha")
+let removename  = name.pop()
+name.push("Imran")
+console.log(name)
+console.log(oneName)
+
+
+let num = [0,1,2,3,4,5,6,7]
+
+num.push(8,9,10)
+
+
+console.log(num)
+ num.shift()
+ num.shift()
+ num.shift()
+ num.shift()
+ num.shift()
+ num.shift()
+ num.shift()
+
+
+ console.log(num)
+
+
+
